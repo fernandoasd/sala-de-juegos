b3ecdb1c87fcd774ac11dd3079125d0835cc61d2
“Fernando_Malinowski” <fernandojose595@gmail.com>
2025-05-17 21:27:23 -0300

    bootstrap rel preload bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,3 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css';
-document.head.appendChild(link);
