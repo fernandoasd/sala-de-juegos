6d98caefceaac7ddb4973bb6f88ef05ad27f69f7
“Fernando_Malinowski” <fernandojose595@gmail.com>
2025-05-17 21:32:45 -0300

    CND bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,8 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css';
+document.head.appendChild(link);
