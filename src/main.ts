22a7aa9ded6e7009532d3c9ec302adcc224860e3
“Fernando_Malinowski” <fernandojose595@gmail.com>
2025-05-17 21:13:31 -0300

    se cargan estilos

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-const script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js';
-script.async = true;
-document.body.appendChild(script);
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css';
+document.head.appendChild(link);
